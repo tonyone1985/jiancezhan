8028c0aa860b861a3c90230a5f911fdb9da03a3c
cxx <32211352@qq.com>
2018-11-21 17:09:23 +0800

    c

---
+++ main.go
@@ -0,0 +1,55 @@
+// webbase project main.go
+package main
+
+import (
+	"fmt"
+
+	//"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+	//	router.Static("/css", "views/css")
+	//	router.Static("/images", "views/images")
+	//	router.Static("/js", "views/js")
+	//	router.Static("/editor", "views/editor")
+	//	router.Static("/src", "views/src")
+
+	router.Static("/", "views")
+
+	//	router.GET("/.html", func(c *gin.Context) {
+	//		c.HTML(http.StatusOK, "", gin.H{
+	//			"title": "Posts",
+	//		})
+	//	})
+	/*
+		router.LoadHTMLGlob("views/pages/*")
+
+		router.GET("/login", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "login.html", gin.H{
+				"title": "Posts",
+			})
+		})
+		router.GET("/form", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "form.html", gin.H{
+				"title": "Users",
+			})
+		})
+		router.GET("/main.html", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "main.html", gin.H{
+				"title": "Users",
+			})
+		})
+		router.GET("/main.html", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "main.html", gin.H{
+				"title": "Users",
+			})
+		})
+	*/
+	router.Run(":8080")
+
+	fmt.Println("Hello World!")
+}
